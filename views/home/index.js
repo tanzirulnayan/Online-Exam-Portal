d1980b93fa4af8cd731d817413a0bc6d11269fdc
Md. Billal Hossain Mozumder <mozumder0204@gmail.com>
2019-02-23 02:09:50 +0600

    node

---
+++ views/home/index.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {      // Wait for the HTML to finish loading.
+//   var resize = function() {
+//     var height = $(window).height();  // Get the height of the browser window area.
+//     var element = $("body");          // Find the element to resize.
+//     element.height(height);           // Set the element's height.
+//   }
+//   resize();
+//   $(window).bind("resize", resize);
+// });
