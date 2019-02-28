8c95815d6e7e85ce243feeb66854101ec4853eb0
Md. Billal Hossain Mozumder <mozumder0204@gmail.com>
2019-03-01 01:31:40 +0600

    node

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var express 		= require('express');
 var bodyParser 		= require('body-parser');
 var exSession 		= require('express-session');
+var path			= require('path');
+var multer 			= require('multer');
 var cookieParser 	= require('cookie-parser');
 var login			= require('./controllers/login');
 var signup			= require('./controllers/signup');
@@ -17,6 +19,15 @@ app.set('view engine', 'ejs');
 app.use(exSession({secret: 'my top secret code', saveUninitialized: true, resave: false}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+var storage = multer.diskStorage({
+	destination: './images/',
+	filename   : function(req,file,cb){
+		cb(null , "image_"+Date.now()+path.extname(file.originalname));
+	}
+});
+app.use(multer({
+	storage:storage
+}).single('imageFile'));
 app.use('/login', login);
 app.use('/signup', signup);
 app.use('/teacher', teacher);
--- controllers/signup.js
+++ controllers/signup.js
@@ -10,36 +10,44 @@ router.get('/', (req, res)=>{
 
 router.post('/', (req, res)=>{
 	
+
+
+	// console.log(res.req.file.filename);
+	// console.log(res.req.file.destination);
+	// var destination = res.req.file.destination;
+	// var studentImage = 
+	// console.log(studentImage);
+
 	var  user ={
 		studentName 		:req.body.fname,
 		studentEmail 		:req.body.email,
 		studentDOB 			:req.body.dob,
 		studentAddress	 	:req.body.address,
-		studentImage 		:req.body.imageFile,
 		userId 				:req.body.uname,
 		password 			:req.body.pass,
 		type 				:req.body.types,
-		
+		studentImage		:res.req.file.destination+res.req.file.filename
 	};
 
-	userModel.insert(user, function(success){
-		if(success){
-			//res.redirect('/login');
-			studentModel.insert(user, function(success){
-				if(success){
-					res.redirect('/login');
-				}
-				// else{
-				// 	console.log(req.body.imageFile);
-				// 	res.redirect('/signup');
-				// }
-			});
+	// userModel.insert(user, function(success){
+	// 	if(success){
+	// 		res.redirect('/login');
+	// 	}
+	// 	else{
+
+	// 		res.redirect('/signup');
 			
+	// 	}
+	// });
+
+	studentModel.insert(user, function(success){
+		if(success){
+			res.redirect('/login');
 		}
 		else{
-
+			console.log(req.body.imageFile);
 			res.redirect('/signup');
-			
+			console.log(user.studentImage);
 		}
 	});
 
