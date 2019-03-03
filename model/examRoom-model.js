5c7c35982092e5e6d144fb97e3ce8428a44f7899
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-03-03 16:27:10 +0600

    examRoom-model added

---
+++ model/examRoom-model.js
@@ -0,0 +1,45 @@
+var db = require('./db');
+
+module.exports={
+
+    get: function(examId, callback){
+		var sql = "select * from exam_rooms where E_ID = ?";
+		db.getResult(sql, [examId], function(result){
+			callback(result);
+		});
+	},
+	getByTeacherId: function(teacherId, callback){
+		var sql = "select * from exam_rooms where T_ID = ?";
+		db.getResult(sql, [teacherId], function(results){
+			callback(results);
+		});
+	},
+	getAll: function(callback){
+		var sql = "select * from exam_rooms";
+		db.getResult(sql, [], function(results){
+			callback(results);
+		});
+	},
+	insert: function(examRoom, callback){
+		var sql = "insert into exam_rooms values ( ?, ?, ?, ?, ?, ?)";
+		
+		db.execute(sql, [examRoom.examId, examRoom.examTitle, examRoom.examDate, examRoom.examStartTime, examRoom.examEndTime, examRoom.teacherId], function(status){
+			callback(status);
+		});
+	},
+	update: function(examRoom, callback){
+		var sql = "update exam_rooms set E_ID = ?, E_TITLE = ?, E_DATE = ?, E_START_TIME = ?, E_END_TIME = ?, T_ID = ? where E_ID = ?";
+		db.execute(sql, [examRoom.examId, examRoom.examTitle, examRoom.examDate, examRoom.examStartTime, examRoom.examEndTime, examRoom.teacherId, examRoom.examId], function(status){
+			callback(status);
+		});
+	},
+	delete: function(examId, callback){
+		var sql = "delete from exam_rooms where E_ID = ?";
+		db.execute(sql, [examId], function(status){
+			callback(status);
+		});
+	},
+}
+
+
+
