cac1f47027e629da42041795ae2e373de311e6ab
Turzz <sakibhossain75@outlook.com>
2019-03-04 20:21:15 +0600

    node

--- controllers/admin.js
+++ controllers/admin.js
@@ -36,7 +36,105 @@ router.post('/profile', (req, res)=>{
 
 });	
 
+//********************************************
+//*************account************************
+router.get('/account', (req, res)=>{
 
+	adminModel.get(req.session.uId, function(result){
+		res.render('admin/account', result[0]);	
+	});	
+});
+
+router.post('/account', (req, res)=>{
+
+});	
+
+
+//********************************************
+//*************Edit_profile************************
+
+
+
+router.get('/edit_profile', (req, res)=>{
+
+	adminModel.get(req.session.uId, function(result){
+		res.render('admin/edit_profile', result[0]);	
+	});	
+});
+
+router.post('/edit_profile', (req, res)=>{
+
+});	
+
+//********************************************
+//*************admin_list************************
+
+
+
+router.get('/admin_list', (req, res)=>{
+
+	adminModel.get(req.session.uId, function(result){
+		res.render('admin/admin_list', result[0]);	
+	});	
+});
+
+router.post('/admin_list', (req, res)=>{
+
+});	
+
+
+
+//********************************************
+//*************pending_list************************
+
+
+
+router.get('/pending_list', (req, res)=>{
+
+	adminModel.get(req.session.uId, function(result){
+		res.render('admin/pending_list', result[0]);	
+	});	
+});
+
+router.post('/pending_list', (req, res)=>{
+
+});	
+
+
+
+//********************************************
+//*************student_list************************
+
+
+
+router.get('/student_list', (req, res)=>{
+
+	adminModel.get(req.session.uId, function(result){
+		res.render('admin/student_list', result[0]);	
+	});	
+});
+
+router.post('/student_list', (req, res)=>{
+
+});	
+
+
+
+//********************************************
+//*************teacher_list************************
+
+
+
+router.get('/teacher_list', (req, res)=>{
+
+	adminModel.get(req.session.uId, function(result){
+		res.render('admin/teacher_list', result[0]);	
+	});	
+});
+
+router.post('/teacher_list', (req, res)=>{
+
+});	
 
 
 
