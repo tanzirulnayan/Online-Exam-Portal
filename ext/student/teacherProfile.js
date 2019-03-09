6f0a614e4b2834009f6a6d2d33e942028aec7e3b
Md. Billal Hossain Mozumder <mozumder0204@users.noreply.github.com>
2019-03-09 10:24:12 +0600

    node

--- ext/student/teacherProfile.js
+++ ext/student/teacherProfile.js
@@ -1,30 +1,31 @@
 $(document).ready(()=>{
 
-    $('#searchbtn').click(()=>{  
-        var id = $('#tsearch').val();
+$('#searchbtn').click(()=>{  
+    var id = $('#tsearch').val();
 
-        $.ajax({
+    $.ajax({
             url: '/student/teacherProfiles/' + id ,
-            method:'get',
+            // data: "",
+            // type: "GET",
+            // contentType: "application/json; charset=utf-8",
+            // dataType: "json",
             success: function(response) {
 
-                alert(response.T_ID +" "+ response.T_NAME+" "+response.T_EMAIL+" "
-                +response.T_MOBILE +" "+response.T_ADDRESS);
+                // alert(response.T_ID +" "+ response.T_NAME+" "+response.T_EMAIL+" "
+                // +response.T_MOBILE +" "+response.T_ADDRESS);
 
                 var views = $('#getall');
 
-                views.html('<h1>' +response.T_NAME+'</h1>');
-
-                
-
-
-
-
-               
-
-            }
-        });
+                views.html(
+                                '<h2>' +response.T_ID+'</h2>\
+                                <h2>' +response.T_NAME+'</h2>\
+                                <h2>' +response.T_EMAIL+'</h2>\
+                                <h2>' +response.T_MOBILE+'</h2>\
+                                <h2>' +response.T_ADDRESS+'</h2>'
+                );   
+        }
     });
+});
 });
 
 
