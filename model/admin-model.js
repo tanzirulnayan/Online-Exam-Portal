73676ea14c3ca9112c7ab2bdf2b19b4bd400126f
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-07-09 14:46:23 +0600

    admin-model formatted

--- model/admin-model.js
+++ model/admin-model.js
@@ -1,51 +1,40 @@
 var db = require('./db');
 
-module.exports={
-
-	get: function(adminId, callback){
+module.exports = {
+	get: function (adminId, callback) {
 		var sql = "select * from admins where A_ID = ?";
-		db.getResult(sql, [adminId], function(result){
+		db.getResult(sql, [adminId], function (result) {
 			callback(result);
 		});
 	},
-	getAll: function(callback){
+	getAll: function (callback) {
 		var sql = "select * from admins";
-		db.getResult(sql, [], function(results){
+		db.getResult(sql, [], function (results) {
 			callback(results);
 		});
 	},
-
-
-
-	insert: function(admin, callback){
+	insert: function (admin, callback) {
 		var sql = "insert into admins values ( ?, ?, ?, ?, ?, ?)";
-		
-		db.execute(sql, [admin.userId, admin.adminName, admin.adminEmail, admin.adminMobile, admin.adminAddress, admin.adminImage], function(status){
+		db.execute(sql, [admin.userId, admin.adminName, admin.adminEmail, admin.adminMobile, admin.adminAddress, admin.adminImage], function (status) {
 			callback(status);
 		});
 	},
-	update: function(admin, callback){
+	update: function (admin, callback) {
 		var sql = "update admins set  A_NAME = ?, A_EMAIL = ?, A_MOBILE = ?, A_ADDRESS = ? where A_ID = ?";
-		
-		db.execute(sql, [ admin.adminName, admin.adminEmail, admin.adminMobile, admin.adminAddress,  admin.adminId], function(status){
+		db.execute(sql, [admin.adminName, admin.adminEmail, admin.adminMobile, admin.adminAddress, admin.adminId], function (status) {
 			callback(status);
 		});
 	},
-	delete: function(adminId, callback){
+	delete: function (adminId, callback) {
 		var sql = "delete from admins where A_ID = ?";
-		db.execute(sql, [adminId], function(status){
+		db.execute(sql, [adminId], function (status) {
 			callback(status);
 		});
 	},
-	
-	pictureedit: function(edit, callback){
+	pictureedit: function (edit, callback) {
 		var sql = "update admins set  A_IMAGE = ? where A_ID = ?";
-		db.execute(sql, [edit.adminImage, edit.adminId], function(status){
+		db.execute(sql, [edit.adminImage, edit.adminId], function (status) {
 			callback(status);
 		});
-	},
-
-
+	}
 }
-
-
