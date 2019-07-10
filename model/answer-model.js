493a42ec95ebebb227ae759f0688d389549777ed
Tanzirul Haque <tanzirul.haque@outlook.com>
2019-07-10 10:27:37 +0600

    answer-model formatted

--- model/answer-model.js
+++ model/answer-model.js
@@ -1,32 +1,29 @@
 var db = require('./db');
 
-module.exports={
-	get: function(answerId, callback){
+module.exports = {
+	get: function (answerId, callback) {
 		var sql = "select * from answers where P_ID = ?";
-		db.getResult(sql, [answerId], function(result){
-			callback(result);
-		});
-    },
-    getByExamId: function(examId, callback){
-		var sql = "select * from answers where E_ID = ? ORDER BY Q_ID";
-		db.getResult(sql, [examId], function(result){
+		db.getResult(sql, [answerId], function (result) {
 			callback(result);
 		});
 	},
-	getAll: function(callback){
+	getByExamId: function (examId, callback) {
+		var sql = "select * from answers where E_ID = ? ORDER BY Q_ID";
+		db.getResult(sql, [examId], function (result) {
+			callback(result);
+		});
+	},
+	getAll: function (callback) {
 		var sql = "select * from answers";
-		db.getResult(sql, [], function(results){
+		db.getResult(sql, [], function (results) {
 			callback(results);
 		});
 	},
-	insert: function(answers, callback){
+	insert: function (answers, callback) {
 		var sql = "insert into answers values (?, ?, ?, ?)";
-		
-		db.execute(sql, [answers.P_ID, answers.E_ID, answers.Q_ID, answers.ANSWER], function(status){
+
+		db.execute(sql, [answers.P_ID, answers.E_ID, answers.Q_ID, answers.ANSWER], function (status) {
 			callback(status);
 		});
 	}
 }
-
-
-
